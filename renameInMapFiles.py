63d2f3efd8c4ddf6f15e3e9b12de20eb3b78b6c7
Jori Romans <jromans@ualberta.ca>
2018-12-04 22:18:34 -0700

    Fixed all picture parameters, hopefully...

--- renameInMapFiles.py
+++ renameInMapFiles.py
@@ -1,10 +1,31 @@
 import os
+import string
+import fileinput
 
 def getPicturesThatWereChanged():
     file = open('changedImages.txt', "r")
     images = []
+    newImages = []
     for line in file:
-        image = line.substring(0, 8)
-        print(image)
+        images += [line[8:line.find(".png")]]
 
-getPicturesThatWereChanged()
+        newImage = line[line.find(".png") + 8:]
+        newImages += [newImage[:newImage.find(".png")]]
+    return (images, newImages)
+
+def renamePhotosInFile(pictureNames):
+    os.chdir('data')
+    files = os.listdir()
+
+    oldNames = pictureNames[0]
+    newNames = pictureNames[1]
+
+    numOfPics = len(oldNames)
+
+    for i in range(0, numOfPics):
+        os.system("sed -i -- 's/" + oldNames[i] + "/" + newNames[i] + "/g' *")
+        print("Replaced " + oldNames[i])
+
+
+
+renamePhotosInFile(getPicturesThatWereChanged())
